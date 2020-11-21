14c2ff275779eaa6211c06ece0f080633e7f3f1a
chikechris <cchike2017@gmail.com>
2020-11-21 16:02:58 -0500

    created compont folder, navbar js file

---
+++ src/Components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-container container" >
+        <Link to='/' className="navbar-logo">
+          Watches
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
