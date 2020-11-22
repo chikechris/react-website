a894143c24e7bee3d84946b621a40909ac781c07
chikechris <cchike2017@gmail.com>
2020-11-22 04:37:19 -0500

    built out button compnt, and css styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import Navbar from './Components/Navbar' 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' />
+        
+      </Switch>
+    </Router>
   )
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,50 @@
-import React from 'react'
+import React, {useState} from 'react' 
+import {Link}from 'react-router-dom' 
+import {MdWatch} from 'react-icons/md'
+import {FaBars, FaTimes} from 'react-icons/fa'
 
 const Navbar = () => {
+  const [click, setClick] = useState(false) 
+
+  const handleClick = () => setClick(!click)
+
+
   return (
     <div className='navbar'>
       <div className="navbar-container container" >
         <Link to='/' className="navbar-logo">
+          <MdWatch className='navbar-icon' />
           Watches
         </Link>
+        <div className='menu-icon' onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <Link to='/' className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/services' className="nav-links">
+              Services
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to='/products' className="nav-links">
+              Products
+            </Link>
+          </li>
+          <li className="nav-btn">
+            { button ? (
+              <Link to='/sign-up' className="btn-link">
+                <Button buttonStyle='btn--outline'>Sing Up</Button>
+              </Link>
+            ) : <Link to='/sign-up'  className="btn-link">
+              <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>Sign Up</Button>
+            </Link>  }
+          </li>
+        </ul>
       </div>
     </div>
   )
